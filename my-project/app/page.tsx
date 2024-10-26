aefc77bd523ef99f8cffd18294348ba2d8c0d65a
agarogo <rewerewu@gmail.com>
2024-10-26 11:26:10 +0900

    site hello world

---
+++ my-project/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-center h-10 mt-[400px]">
+      <h1 className="text-red-400">HELLO WORLD BOGFED</h1>
+      <div className="h-[1px] bg-black mt-10"></div>
+    </div>
+  );
+}
